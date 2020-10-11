5271271129878668613f791bc6397caff110aacc
Hitesh Rana <always90s@yahoo.com>
2020-10-11 14:49:34 -0700

    added addItem, removeItem functionality to checkout item component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { selectCartItems, selectCartTotal } from "../../redux/cart/cart.selectors";
-import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../redux/cart/cart.selectors";
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 
 import "./checkbox.styles.scss";
 
-const CheckoutPage = ({cartItems, total}) => {
+const CheckoutPage = ({ cartItems, total }) => {
   return (
     <div className="checkout-page">
       <div className="checkout-header">
@@ -27,17 +30,19 @@ const CheckoutPage = ({cartItems, total}) => {
           <span>Remove</span>
         </div>
       </div>
-      {
-          cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem} />) 
-      }
-      <div className='total'><span>TOTAL: ${total}</span></div>
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <div className="total">
+        <span>TOTAL: ${total}</span>
+      </div>
     </div>
   );
 };
 
 const mapStateToProps = createStructuredSelector({
-    cartItems: selectCartItems,
-    total: selectCartTotal
-})
+  cartItems: selectCartItems,
+  total: selectCartTotal,
+});
 
 export default connect(mapStateToProps, null)(CheckoutPage);
